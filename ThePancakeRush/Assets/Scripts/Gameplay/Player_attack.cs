2852ca3ddca94c97b10aecb64d177d57932c7109
Ancuta Catalin Mihai <catalin_puiu11@yahoo.com>
2021-04-10 14:33:20 +0300

    Caracter attack

---
+++ ThePancakeRush/Assets/Scripts/Gameplay/Player_attack.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_attack : MonoBehaviour
+{
+ 	public Animator animator;
+
+ 	public Transform punctDeAtac;
+ 	public float razaDeAtac = 0.5f;
+ 	public LayerMask feluriDeInamici;
+
+ 	public int valoareLovitura = 40;
+
+ 	public float rataDeAtac = 2f;
+ 	float timpulPanaLaUrmatorulAtac = 0f;
+    // Update is called once per frame
+    void Update()
+    {
+    	if(Time.time >= timpulPanaLaUrmatorulAtac){
+			if(Input.GetKeyDown(KeyCode.S)){
+				Attack();
+				timpulPanaLaUrmatorulAtac = Time.time + 2f / rataDeAtac;
+			}        
+		}
+    }
+
+    void Attack(){
+    	//porneste animatia cand este apasat butonul
+    	animator.SetTrigger("Attack");
+
+    	//detecteaza inamicii in raza de atac
+		Collider2D[] inamiciLoviti  =  Physics2D.OverlapCircleAll(punctDeAtac.position, razaDeAtac, feluriDeInamici);
+
+		//loveste inamicii
+		foreach(Collider2D inamic in inamiciLoviti) inamic.GetComponent<Slime_hit>().TakeDamage(valoareLovitura);
+    }
+
+    void OnDrawGizmosSelected(){
+    	if(punctDeAtac == null) return;
+
+    	Gizmos.DrawWireSphere(punctDeAtac.position, razaDeAtac);
+    }
+}
---
+++ ThePancakeRush/Assets/Slime_hit.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Slime_hit : MonoBehaviour
+{
+
+	public Animator animator;
+
+	public int viataMaxima = 100;
+	int viataRamasa;
+    // Start is called before the first frame update
+    void Start()
+    {
+        viataRamasa = viataMaxima; 
+    }
+
+    public void TakeDamage(int lovitura){
+    	viataRamasa -= lovitura;
+
+    	animator.SetTrigger("Lovit");
+
+    	if(viataRamasa <= 0){
+    		Moarte();
+    	}
+    }
+
+    void Moarte(){
+    	Debug.Log("Inamicul a murit");
+
+    	//Animatia de moarte
+    	animator.SetBool("esteMort",true);
+    
+    	//Disable inamic
+    	GetComponent<Collider2D>().enabled = false;
+    	this.enabled = false;
+    }
+
+}
