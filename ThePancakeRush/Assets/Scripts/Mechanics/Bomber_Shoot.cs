d84be0c29f69eaa77d2b5c0c3e2a53508687904c
Tudorache Mihai-Ovidiu <ovi99or@gmail.com>
2021-04-10 20:19:34 +0300

    Added bombs as projectiles for the bomber

---
+++ ThePancakeRush/Assets/Scripts/Mechanics/Bomber_Shoot.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bomber_Shoot : MonoBehaviour
+{
+    public Transform firePoint;
+    public GameObject bombPrefab;
+    Transform player;
+    Rigidbody2D rb;
+
+    public float attackRange = 3f;
+    public float attackDelay = 1f;
+    private float lastAttackTime;
+
+    // Use this for initialization
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Vector2.Distance(player.position, rb.position) <= attackRange)
+        {
+            if(Time.time > lastAttackTime + attackDelay)
+            {
+                Shoot();
+                lastAttackTime = Time.time;
+            }
+        }
+    }
+
+    void Shoot()
+    {
+        Instantiate(bombPrefab, firePoint.position, firePoint.rotation);
+    }
+}
