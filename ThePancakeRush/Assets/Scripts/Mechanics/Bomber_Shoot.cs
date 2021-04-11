615720c09562e5e0735d45a509d978755ebd7b61
Tudorache Mihai-Ovidiu <ovi99or@gmail.com>
2021-04-11 20:56:05 +0300

    Added animation event for bomber shooting

--- ThePancakeRush/Assets/Scripts/Mechanics/Bomber_Shoot.cs
+++ ThePancakeRush/Assets/Scripts/Mechanics/Bomber_Shoot.cs
@@ -6,32 +6,6 @@ public class Bomber_Shoot : MonoBehaviour
 {
     public Transform firePoint;
     public GameObject bombPrefab;
-    Transform player;
-    Rigidbody2D rb;
-
-    public float attackRange = 3f;
-    public float attackDelay = 1f;
-    private float lastAttackTime;
-
-    // Use this for initialization
-    void Start()
-    {
-        player = GameObject.FindGameObjectWithTag("Player").transform;
-        rb = GetComponent<Rigidbody2D>();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if(Vector2.Distance(player.position, rb.position) <= attackRange)
-        {
-            if(Time.time > lastAttackTime + attackDelay)
-            {
-                Shoot();
-                lastAttackTime = Time.time;
-            }
-        }
-    }
 
     void Shoot()
     {
