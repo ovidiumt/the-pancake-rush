a5314dde86cff63aac210e48d92919ae9c4a9f49
Tudorache Mihai-Ovidiu <ovi99or@gmail.com>
2021-04-27 12:24:35 +0300

    Added script for collectables

---
+++ ThePancakeRush/Assets/Scripts/Core/Collectable.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectable : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        Destroy(gameObject);
+    }
+}
