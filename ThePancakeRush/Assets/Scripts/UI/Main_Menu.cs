b7087f64f0f0a155bdcce4591d1e9d26eb876d32
Ancuta Catalin Mihai <catalin_puiu11@yahoo.com>
2021-05-03 12:50:55 +0300

    added loading bar to screen loading

--- ThePancakeRush/Assets/Scripts/UI/Main_Menu.cs
+++ ThePancakeRush/Assets/Scripts/UI/Main_Menu.cs
@@ -1,20 +1,38 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class Main_Menu : MonoBehaviour
 {
-
+	public GameObject loadingScreen;
+	public Slider slider;
+	public TextMeshProUGUI progressText;
 
     public void PlayGame()
     {
-	    SceneManager.LoadScene("Game");
+	    StartCoroutine(LoadSceneAsyncronously("Game"));
+    }
+
+    IEnumerator LoadSceneAsyncronously(string name){
+    	AsyncOperation operation = SceneManager.LoadSceneAsync(name);
+
+    	loadingScreen.SetActive(true);
+
+    	while(!operation.isDone){
+    		float progress = Mathf.Clamp01(operation.progress / .9f);
+
+    		slider.value = progress;
+    		progressText.text = progress * 100f + "%";
+
+    		yield return null;
+    	}
     }
 
     public void QuitGame()
     {
-        Debug.Log("QUIT!");
         Application.Quit();
     }
 }
