f1c1af69cd56c172125b3fd871cdbff4ec719c02
Ancuta Catalin Mihai <catalin_puiu11@yahoo.com>
2021-05-09 12:42:02 +0300

    Added Music to Game

---
+++ ThePancakeRush/Assets/Scripts/Core/AudioManager.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine.Audio;
+using System;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+	public Sounds[] sounds;
+	public Transform player;
+	private bool is_Playingsecond = false, is_Playingfirst = false;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        foreach(Sounds s in sounds){
+        	s.source = gameObject.AddComponent<AudioSource>();
+        	s.source.clip = s.clip;
+        	s.source.volume = s.volume;
+        	s.source.pitch = s.pitch;
+        	s.source.loop = s.loop;
+        }
+    }
+
+    void Start(){
+    	Play("MainTheme"); 
+    	is_Playingfirst = true;
+    }
+
+    void Update(){
+    	if(player.position.y <= -2 && !is_Playingsecond) {
+    		Stop("MainTheme");
+    		Play("CaveTheme");
+    		is_Playingsecond = true;
+    		is_Playingfirst = false;
+    	}else if(player.position.y >= -2 && !is_Playingfirst){
+    		Stop("CaveTheme");
+    		Play("MainTheme");
+    		is_Playingsecond = false;
+    		is_Playingfirst = true;
+    	}
+    }
+
+    public void Play(string name){
+    	Sounds s = Array.Find(sounds, sound => sound.name == name);
+
+    	if(s == null) return;
+
+    	s.source.Play();
+    }
+
+    public void Stop(string name){
+    	Sounds s = Array.Find(sounds, sound => sound.name == name);
+
+    	if(s == null) return;
+
+    	s.source.Stop();
+    }
+}
