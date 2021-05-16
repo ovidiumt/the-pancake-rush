3757e889db484ae05256c8f0e45fe8c51fb4b345
Ancuta Catalin Mihai <catalin_puiu11@yahoo.com>
2021-05-16 14:58:40 +0300

    added healthPotion test

---
+++ ThePancakeRush/Assets/EditMode/HealthPotion.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEditor;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class HealthPotion
+    {
+        private GameObject healthPotion = AssetDatabase.LoadAssetAtPath<GameObject>("Assets/Models/Collectable/Prefabs/Health_Potion.prefab");
+        // A Test behaves as an ordinary method
+        [Test]
+        public void HealthPotionHealingValue()
+        {
+            var valueOfHealing = healthPotion.GetComponent<Health_Potion>().healingValue;
+
+            Assert.AreEqual(20, valueOfHealing);
+        }
+
+    }
+}
